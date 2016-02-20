9c02089bfdda2f504d2562b9aa25aa6e1d959748
Ole-Petter Olsen <opolsen@gmail.com>
2016-02-20 01:00:01 +0100

    Add keybindings for opening line above and below

--- customizations/editing.el
+++ customizations/editing.el
@@ -115,3 +115,19 @@
             (lambda ()
                   (interactive)
                   (join-line -1)))
+
+(defun open-line-below ()
+  (interactive)
+  (end-of-line)
+  (newline)
+  (indent-for-tab-command))
+
+(defun open-line-above ()
+  (interactive)
+  (beginning-of-line)
+  (newline)
+  (forward-line -1)
+  (indent-for-tab-command))
+
+(global-set-key (kbd "<C-return>") 'open-line-below)
+(global-set-key (kbd "<C-S-return>") 'open-line-above)
