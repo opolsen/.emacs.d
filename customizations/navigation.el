d65e2cdc42a6b2b7ac9da261a9434bfb338c0862
Ole-Petter Olsen <opolsen@gmail.com>
2016-03-17 20:44:02 +0100

    Add keybinding for smex-major-mode-commands

--- customizations/navigation.el
+++ customizations/navigation.el
@@ -67,6 +67,7 @@
 (setq smex-save-file (concat user-emacs-directory ".smex-items"))
 (smex-initialize)
 (global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
 
 ;; projectile everywhere!
 (projectile-global-mode)
