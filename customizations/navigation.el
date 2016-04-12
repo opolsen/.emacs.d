da5bba4370d8f514268740ce7219573ce59d7b57
Ole-Petter Olsen <Ole-Petter.Olsen@klp.no>
2016-04-12 08:09:41 +0200

    Add keybindings for moving 5 lines/chars at a time

--- customizations/navigation.el
+++ customizations/navigation.el
@@ -84,6 +84,28 @@
 (global-set-key (kbd "C-<home>") 'beginning-of-buffer)
 (global-set-key (kbd "C-<end>") 'end-of-buffer)
 
+;; Move more quickly
+(global-set-key (kbd "C-S-n")
+                (lambda ()
+                  (interactive)
+                  (ignore-errors (next-line 5))))
+
+(global-set-key (kbd "C-S-p")
+                (lambda ()
+                  (interactive)
+                  (ignore-errors (previous-line 5))))
+
+(global-set-key (kbd "C-S-f")
+                (lambda ()
+                  (interactive)
+                  (ignore-errors (forward-char 5))))
+
+(global-set-key (kbd "C-S-b")
+                (lambda ()
+                  (interactive)
+                  (ignore-errors (backward-char 5))))
+
+
 ;; Displays the available key bindings after opening sequence
 (require 'which-key)
 (which-key-mode)
