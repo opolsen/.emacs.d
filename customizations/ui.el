cfd195a64af9c1737fa6a60614a592ad69e6db08
Ole-Petter Olsen <Ole-Petter.Olsen@klp.no>
2016-05-13 15:29:39 +0200

    Use magit in fullscreen

--- customizations/ui.el
+++ customizations/ui.el
@@ -97,3 +97,17 @@
 (eval-after-load "tern" '(diminish 'tern-mode))
 (eval-after-load "company" '(diminish 'company-mode))
 (eval-after-load "autorevert" '(diminish 'auto-revert-mode))
+
+;; full screen magit-status
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+(defun magit-quit-session ()
+  "Restores the previous window configuration and kills the magit buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+
+(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
