5189e5de8e1111caf9d02366b7d1fbf249f39dda
Ole-Petter Olsen <Ole-Petter.Olsen@klp.no>
2016-05-23 09:23:39 +0200

    Add keybinding for #'crux-kill-whole-line

--- customizations/navigation.el
+++ customizations/navigation.el
@@ -165,3 +165,4 @@
 (global-set-key (kbd "C-c D") #'crux-delete-file-and-buffer)
 (global-set-key (kbd "C-c r") #'crux-rename-file-and-buffer)
 (global-set-key (kbd "C-c d") #'crux-duplicate-current-line-or-region)
+(global-set-key (kbd "s-k") #'crux-kill-whole-line)
