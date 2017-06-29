e9dc1b5231f2f4a2d9b7e438bc96136cfb92bb4a
Ole-Petter Olsen <opolsen@gmail.com>
2017-06-29 21:44:33 +0200

    Delete color themes
    Use color theme from package instead.

--- customizations/themes.el
+++
@@ -1,4 +0,0 @@
-;; Themes
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-(add-to-list 'load-path "~/.emacs.d/themes")
-(load-theme 'tomorrow-night-bright t)
--- customizations/ui.el
+++ customizations/ui.el
@@ -15,13 +15,6 @@
 (when (fboundp 'scroll-bar-mode)
   (scroll-bar-mode -1))
 
-;; Color Themes
-;; Read http://batsov.com/articles/2012/02/19/color-theming-in-emacs-reloaded/
-;; for a great explanation of emacs color themes.
-;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Custom-Themes.html
-;; for a more technical explanation.
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-(add-to-list 'load-path "~/.emacs.d/themes")
 (load-theme 'zerodark t)
 
 (set-face-attribute 'default nil :height 110 :family "Source Code Pro Medium")
