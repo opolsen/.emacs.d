63e4980a6b5782bf2c8904a292aecfd0e865d995
Ole-Petter Olsen <opolsen@gmail.com>
2017-09-19 10:58:42 +0200

    Setup company-anaconda in python hook

--- customizations/setup-python.el
+++ customizations/setup-python.el
@@ -9,15 +9,15 @@
   (anaconda-eldoc-mode)
   (subword-mode)
   (company-mode)
+  (add-to-list 'company-backends 'company-anaconda)
   (smartparens-mode)
   (flycheck-mode)
   (when (executable-find "pylint")
     (flycheck-select-checker 'python-pylint)))
 
-(eval-after-load "company"
-  '(add-to-list 'company-backends 'company-anaconda))
 
 (add-hook 'python-mode-hook 'my-python-mode-hook)
 (add-hook 'inferior-python-mode-hook 'smartparens-mode)
+
 
 (define-key python-mode-map (kbd "C-c v") 'pythonic-activate)
