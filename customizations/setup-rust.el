3b7fd336d887d26383bd98eccfbd64631aa1afee
Ole-Petter Olsen <opolsen@gmail.com>
2017-09-19 18:20:21 +0200

    Cleanup rust setup

--- customizations/setup-rust.el
+++ customizations/setup-rust.el
@@ -1,20 +1,15 @@
+(require 'company)
+(require 'company-racer)
+(require 'flycheck)
+
 (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
 
-;; Racer cannot read RUST_SRC_PATH in emacs for some reason,
-;; so setting this explicitly for now
-(setq racer-rust-src-path "~/rustc-1.8.0/src")
-
-;; RACER adds additional code completion and navigation
-(add-hook 'rust-mode-hook #'racer-mode)
-(add-hook 'racer-mode-hook #'eldoc-mode)
-
-(add-hook 'racer-mode-hook #'flycheck-mode)
-(add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
-
-;; Use company for auto-completion
-(require 'company-racer)
-(with-eval-after-load 'company
+(defun my-rust-mode-hook ()
+  (racer-mode) ;; adds additional code completion and navigation
+  (eldoc-mode)
+  (flycheck-mode)
+  (flycheck-rust-setup)
+  (company-mode)
   (add-to-list 'company-backends 'company-racer))
 
-(add-hook 'racer-mode-hook #'company-mode)
-(setq company-tooltip-align-annotations t)
+(add-hook 'rust-mode-hook #'my-rust-mode-hook)
