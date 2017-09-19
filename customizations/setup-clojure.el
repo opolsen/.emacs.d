a71783289660aa93eb5ab8df3967e05adb1c5a11
Ole-Petter Olsen <opolsen@gmail.com>
2017-09-19 23:08:36 +0200

    Cleanup setup for some modes

--- customizations/setup-clojure.el
+++ customizations/setup-clojure.el
@@ -1,49 +1,20 @@
-;;;;
-;; Clojure
-;;;;
-
-;; Enable smartparens
-(add-hook 'clojure-mode-hook 'smartparens-strict-mode)
-
-;; This is useful for working with camel-case tokens, like names of
-;; Java classes (e.g. JavaClassName)
-(add-hook 'clojure-mode-hook 'subword-mode)
-
-;; A little more syntax highlighting
 (require 'clojure-mode-extra-font-locking)
-
-;; syntax hilighting for midje
-(add-hook 'clojure-mode-hook
-          (lambda ()
-            (setq inferior-lisp-program "lein repl")
-            (font-lock-add-keywords
-             nil
-             '(("(\\(facts?\\)"
-                (1 font-lock-keyword-face))
-               ("(\\(background?\\)"
-                (1 font-lock-keyword-face))))
-            (define-clojure-indent (fact 1))
-            (define-clojure-indent (facts 1))))
-
 (require 'clj-refactor)
 
 (defun my-clojure-mode-hook ()
-    (clj-refactor-mode 1)
-    (yas-minor-mode 1) ; for adding require/use/import statements
-    ;; This choice of keybinding leaves cider-macroexpand-1 unbound
-    (cljr-add-keybindings-with-prefix "C-c C-m")
-    (flycheck-mode 1)
-    (flycheck-clojure-setup))
+  (clj-refactor-mode 1)
+  (smartparens-strict-mode)
+  (subword-mode)
+  (cljr-add-keybindings-with-prefix "C-c C-m")
+  (flycheck-mode 1)
+  (flycheck-clojure-setup))
 
 (add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
 
-
-;;;;
-;; Cider
-;;;;
-
-;; provides minibuffer documentation for the code you're typing into the repl
 (add-hook 'cider-mode-hook 'eldoc-mode)
+
+(add-hook 'cider-repl-mode-hook 'smartparens-mode)
+(add-hook 'cider-repl-mode-hook 'subword-mode)
 
 ;; hide REPL buffer when it's finished connecting
 (setq cider-repl-pop-to-buffer-on-connect nil)
@@ -58,39 +29,5 @@
 ;; Wrap when navigating history.
 (setq cider-repl-wrap-history t)
 
-;; enable paredit in your REPL
-(add-hook 'cider-repl-mode-hook 'paredit-mode)
-
-;; Use clojure mode for other extensions
-(add-to-list 'auto-mode-alist '("\\.edn$" . clojure-mode))
-(add-to-list 'auto-mode-alist '("\\.boot$" . clojure-mode))
-(add-to-list 'auto-mode-alist '("lein-env" . enh-ruby-mode))
-
-
-;; key bindings
-;; these help me out with the way I usually develop web apps
-(defun cider-start-http-server ()
-  (interactive)
-  (cider-load-current-buffer)
-  (let ((ns (cider-current-ns)))
-    (cider-repl-set-ns ns)
-    (cider-interactive-eval (format "(println '(def server (%s/start))) (println 'server)" ns))
-    (cider-interactive-eval (format "(def server (%s/start)) (println server)" ns))))
-
-
-(defun cider-refresh ()
-  (interactive)
-  (cider-interactive-eval (format "(user/reset)")))
-
-(defun cider-user-ns ()
-  (interactive)
-  (cider-repl-set-ns "user"))
-
-(eval-after-load 'cider
-  '(progn
-     (define-key clojure-mode-map (kbd "C-c C-v") 'cider-start-http-server)
-     (define-key clojure-mode-map (kbd "C-M-r") 'cider-refresh)
-     (define-key clojure-mode-map (kbd "C-c u") 'cider-user-ns)
-     (define-key cider-mode-map (kbd "C-c u") 'cider-user-ns)))
-
+;; Start figwheel when creating cljs repl
 (setq cider-cljs-lein-repl "(do (use 'figwheel-sidecar.repl-api) (start-figwheel!) (cljs-repl))")
--- customizations/setup-js.el
+++ customizations/setup-js.el
@@ -1,28 +1,40 @@
-;; javascript / html
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-(add-hook 'js2-mode-hook 'js2-imenu-extras-mode)
+(require 'company-tern)
 
-(setq js2-highlight-level 3)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
 
 (defun my-js2-mode-hook ()
-            (flycheck-mode t)
-            (when (executable-find "eslint")
-              (flycheck-select-checker 'javascript-eslint))
-            (tern-mode t)
-            (company-mode t)
-            (define-key js-mode-map (kbd "M-.") nil)
-            (define-key js2-mode-map (kbd "M-j") nil)
-            (define-key js2-mode-map (kbd "C-k") 'js2r-kill))
+  (flycheck-mode t)
+  (when (executable-find "eslint")
+    (flycheck-select-checker 'javascript-eslint))
+  (tern-mode t)
+  (company-mode t)
+  (add-to-list 'company-backends 'company-tern)
+  (js2-imenu-extras-mode)
+  (subword-mode)
+  (smartparens-mode)
+  (js2-refactor-mode)
+  (js2r-add-keybindings-with-prefix "C-c C-m")
+  (define-key js-mode-map (kbd "M-.") nil)
+  (define-key js2-mode-map (kbd "M-j") nil)
+  (define-key js2-mode-map (kbd "C-k") 'js2r-kill))
 
 (add-hook 'js2-mode-hook 'my-js2-mode-hook)
 
-;; Activate subword-mode
-(add-hook 'js-mode-hook 'subword-mode)
-(add-hook 'web-mode-hook 'subword-mode)
+(defun my-web-mode-hook ()
+  (subword-mode)
+  (smartparens-mode)
+  (web-mode-set-content-type "jsx")
+  (flycheck-mode t)
+  (when (executable-find "eslint")
+    (flycheck-add-mode 'javascript-eslint 'web-mode)
+    (tern-mode t)
+    (company-mode t)
+    (add-to-list 'company-backends 'company-tern)))
 
-;; Setup js2-refactor-mode
-(add-hook 'js2-mode-hook #'js2-refactor-mode)
-(js2r-add-keybindings-with-prefix "C-c C-m")
+(add-hook 'web-mode-hook 'my-web-mode-hook)
+
+(setq js2-highlight-level 3)
 
 ;; Set indentation level
 (setq-default js2-basic-offset 2)
@@ -33,31 +45,10 @@
               web-mode-style-padding 2
               web-mode-script-padding 2)
 
-;; Use web-mode for .jsx files
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
-
-;; Use jsx as default content-type
-(defun my-web-mode-hook ()
-  (web-mode-set-content-type "jsx")
-  (flycheck-mode t)
-  (when (executable-find "eslint")
-    (flycheck-add-mode 'javascript-eslint 'web-mode)
-    (tern-mode t)
-    (company-mode t)))
-
-(add-hook 'web-mode-hook 'my-web-mode-hook)
-
-;; Use smartparens for web modes
-(add-hook 'js-mode-hook #'smartparens-mode)
-(add-hook 'web-mode-hook #'smartparens-mode)
-
 ;; Additional pairs for smartparens
 (sp-with-modes 'web-mode
   (sp-local-pair "<" ">")
   (sp-local-pair "`" "`"))
-
-(require 'company-tern)
-(add-to-list 'company-backends 'company-tern)
 
 ;; From https://emacs.stackexchange.com/questions/33536/how-to-edit-jsx-react-files-in-emacs
 (defadvice js-jsx-indent-line (after js-jsx-indent-line-after-hack activate)
